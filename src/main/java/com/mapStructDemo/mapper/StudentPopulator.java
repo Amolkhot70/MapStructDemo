c87e5db0ef0f54d2f281365f79cc526391046bed
amolkhot70 <amolkhot70@outlook.com>
2025-01-16 18:36:22 +0530

    modified StudentPopulator

--- src/main/java/com/mapStructDemo/mapper/StudentPopulator.java
+++ src/main/java/com/mapStructDemo/mapper/StudentPopulator.java
@@ -4,6 +4,7 @@ import com.mapStructDemo.dto.StudentDto;
 import com.mapStructDemo.entity.Student;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,9 +12,11 @@ public interface StudentPopulator {
 
     StudentPopulator INSTANCE= Mappers.getMapper(StudentPopulator.class);
 
-    @Mapping(target = "id",ignore = true)
-    @Mapping(target = "creationDate",ignore = true)
-    @Mapping(target = "city",source = "landMark")
+   @Mappings({
+           @Mapping(target = "id",ignore = true),
+           @Mapping(target = "creationDate",ignore = true),
+           @Mapping(target = "city",source = "landMark")
+   })
     Student populateStudent(StudentDto studentDto);
 
     StudentDto populateStudentDto(Student student);
